2037b5104d7efc8127c65e63c0c37e621887506b
Merlin-3D <axel.deffo@gmail.com>
2024-05-05 12:28:32 +0100

    fix: config electron.

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
           </AuthProvider>
         </AuthMiddleware>
       </BrowserRouter>
+      <ToastContainer />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(
@@ -17,7 +16,6 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ToastContainer />
     </QueryClientProvider>
   </React.StrictMode>
 );
